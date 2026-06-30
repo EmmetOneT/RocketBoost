2da9b0f4e5347da2a366a9a55e1d7be51effaa51
Emmet Carroll <emmetcarroll94@gmail.com>
2026-06-30 01:59:58 +0100

    Level Fail v1
    + Added collision handler.
    + Scenemanager reset on collision using switch statement.

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("looks good");
+                break;
+            case "Finish":
+                Debug.Log("Finished");
+                break;
+            case "Fuel":
+                Debug.Log("Refuelled");
+             break;
+                default:
+                ReloadLevel();
+                break;
+        }
+
+        void ReloadLevel()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+}
