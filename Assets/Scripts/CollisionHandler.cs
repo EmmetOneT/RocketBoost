323e5a8e75a024b629e515122d1cc68d1791c50b
Emmet Carroll <emmetcarroll94@gmail.com>
2026-07-01 14:19:41 +0100

    Delays + Level Handler
    + Added Load Level
    +Invoke for collision Delay
    +Disable player control
    +Delay on load next level for FX

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,8 +1,12 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float levelLoadDelay = 2f;
     private void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -10,21 +14,52 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("looks good");
                 break;
+
+            //Load next level on landing pad
             case "Finish":
-                Debug.Log("Finished");
+                StartSuccessSequence();
                 break;
             case "Fuel":
                 Debug.Log("Refuelled");
-             break;
-                default:
-                ReloadLevel();
+                break;
+
+            //Reload level on collision
+            default:
+                StartCrashSequence();
+               
                 break;
         }
+    }
 
-        void ReloadLevel()
+    private void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    //Reload scene method
+    void ReloadLevel()
         {
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentScene);
         }
-    }
+
+        //Load next scene method
+        void LoadNextLevel()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            int nextScene = currentScene + 1;
+            if (nextScene == SceneManager.sceneCountInBuildSettings)
+            {
+                nextScene = 0;
+            }
+            SceneManager.LoadScene(nextScene);
+        }
 }
+
